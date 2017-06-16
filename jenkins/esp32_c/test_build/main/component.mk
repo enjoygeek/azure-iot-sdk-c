4cdef2e489be666e1f65083fea297a166266d424
Roy Sprowl <roys@jetstreamsoftware.com>
2017-06-15 17:27:13 -0700

    renamed

--- jenkins/esp32_c/test_build/main/component.mk
+++ jenkins/esp32_c/test_build/main/component.mk
@@ -1,8 +1,18 @@
-#
-# "main" pseudo-component makefile.
-#
-# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-COMPONENT_OBJS = compile_test_main.o
+
+sdk := ../../../../..
+
+COMPONENT_ADD_INCLUDEDIRS :=  \
+$(sdk)/c-utility/inc  \
+$(sdk)/c-utility/inc/azure_c_shared_utility \
+$(sdk)/c-utility/inc/azure_c_shared_utility/lwip \
+$(sdk)/iothub_client/inc \
+$(sdk)/umqtt/inc  \
+$(sdk)/umqtt/inc/azure_umqtt_c 	\
+$(sdk)/parson  \
+$(sdk)/iothub_client/samples/iothub_client_sample_mqtt
+
+COMPONENT_OBJS = main.o
 
 COMPONENT_SRCDIRS := .
+
