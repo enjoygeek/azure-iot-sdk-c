f822ee860882972cb1e588f683eea019cf4b33bd
Roy Sprowl <roys@jetstreamsoftware.com>
2017-06-15 17:03:39 -0700

    ...

--- jenkins/esp32_c/test_build/main/component.mk
+++ jenkins/esp32_c/test_build/main/component.mk
@@ -3,6 +3,6 @@
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
-COMPONENT_OBJS = compile_test_main2.o
+COMPONENT_OBJS = compile_test_main.o
 
 COMPONENT_SRCDIRS := .
